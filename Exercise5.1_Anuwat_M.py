b223992489e7b18326d6e5e51c1a650b5b2d24cb
6587043 <115236714+jamemassem@users.noreply.github.com>
2022-10-08 22:42:01 +0700

    Create Exercise5.1_Anuwat_M.py

---
+++ Exercise5.1_Anuwat_M.py
@@ -0,0 +1,7 @@
+num1 = int(input('Please input the first number : ')) 
+num2 = int(input('Please input the second number : ')) 
+
+print(f'{num1} + {num2} = {num1+num2}') 
+print(f'{num1} - {num2} = {num1-num2}') 
+print(f'{num1} x {num2} = {num1*num2}') 
+print(f'{num1} / {num2} = {num1/num2}')

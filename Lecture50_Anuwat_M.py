c61314bb80a528e3069b33c17076191deab1ea28
6587043 <115236714+jamemassem@users.noreply.github.com>
2022-10-10 09:09:43 +0700

    Create Lecture50_Anuwat_M.py

---
+++ Lecture50_Anuwat_M.py
@@ -0,0 +1,16 @@
+def addnum(x,y): 
+    print(x+y) 
+
+def subtractnum(x,y): 
+    print(x-y) 
+
+def multinum(x,y): 
+    print(x*y) 
+
+def dividenum(x,y): 
+    print(x/y) 
+
+addnum(10,2) 
+subtractnum(10,2) 
+multinum(10,2) 
+dividenum(10,2)
